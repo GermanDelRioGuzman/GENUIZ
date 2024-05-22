eba888855b245d7b9381ff9d2f3ccf9036196cef
ferreyesmtz <167777596+ferreyesmtz@users.noreply.github.com>
2024-05-22 00:30:48 +0000

    Version2

--- back/PASSPORT/server.js
+++ back/PASSPORT/server.js
@@ -1,5 +1,40 @@
 const express = require('express');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const PassportLocal = require('passport-local').Strategy;
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use(cookieParser('secreto'));
+
+app.use(session({
+    secret: 'secreto',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+passport.use(new PassportLocal(function(username, password, done){
+    if (username === "admin" && password === "123")
+        return done(null, { id: 1, name: "Cody" });
+    done(null, false);
+}));
+// { id: 1, name: "Cody" }
+// 1 -> Serializacion
+
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+    done(null, { id: 1, name: "Cody" });
+});
 
 app.set('view engine', 'ejs');
 
