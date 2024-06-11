c5ad6ea44e61f475cab7a4f7fa18dec25eeba963
GermanDelRioGuzman <110877408+GermanDelRioGuzman@users.noreply.github.com>
2024-06-11 21:33:01 +0000

    v2.6

--- src/index.js
+++ src/index.js
@@ -167,6 +167,7 @@ async function main() {
         });
     });
 
+
     // Ruta para manejar la petición POST
     app.listen(port, () => {
         console.log(`Server running on http://localhost:${port}`);
